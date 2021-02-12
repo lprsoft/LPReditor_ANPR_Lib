4668d600556a51a5fee670ec9e4ffc25df8c8608
Raphael Poulenard <raphael.poulenard@free.fr>
2021-02-12 21:47:10 +0100

    Add files via upload

---
+++ sample_cpp/build/CMakeFiles/sample_cpp.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/raphael/Programmation/LPReditor_ANPR_Lib/src/Levenshtein.cpp" "/home/raphael/Programmation/LPReditor_ANPR_Lib/sample_cpp/build/CMakeFiles/sample_cpp.dir/home/raphael/Programmation/LPReditor_ANPR_Lib/src/Levenshtein.cpp.o"
+  "/home/raphael/Programmation/LPReditor_ANPR_Lib/src/utils_image_file.cpp" "/home/raphael/Programmation/LPReditor_ANPR_Lib/sample_cpp/build/CMakeFiles/sample_cpp.dir/home/raphael/Programmation/LPReditor_ANPR_Lib/src/utils_image_file.cpp.o"
+  "/home/raphael/Programmation/LPReditor_ANPR_Lib/sample_cpp/sample_cpp.cpp" "/home/raphael/Programmation/LPReditor_ANPR_Lib/sample_cpp/build/CMakeFiles/sample_cpp.dir/sample_cpp.cpp.o"
+  "/home/raphael/Programmation/LPReditor_ANPR_Lib/sample_cpp/stdafx.cpp" "/home/raphael/Programmation/LPReditor_ANPR_Lib/sample_cpp/build/CMakeFiles/sample_cpp.dir/stdafx.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LPREDITOR_DEMO_NO_ARGS"
+  "LPREDITOR_DEMO_PRINT_STATS_IN_TXT_FILE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/raphael/Programmation/LPReditor_ANPR_Lib"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sample_cpp/build/CMakeFiles/sample_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_cpp.dir/home/raphael/Programmation/LPReditor_ANPR_Lib/src/Levenshtein.cpp.o"
+  "CMakeFiles/sample_cpp.dir/home/raphael/Programmation/LPReditor_ANPR_Lib/src/utils_image_file.cpp.o"
+  "CMakeFiles/sample_cpp.dir/sample_cpp.cpp.o"
+  "CMakeFiles/sample_cpp.dir/stdafx.cpp.o"
+  "sample_cpp"
+  "sample_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
